877439f1f87bb0e1a06ad6cb83b53149161c9b43
georgechou <georgechmr@gmail.com>
2021-10-22 17:54:50 +0800

    update md head

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import os
 
 from github import Github
 
-MD_HEAD = """**<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
-**<p align="center">用于记录一些幼稚的想法和脑残的瞬间~</p>**
+MD_HEAD = """**<p align="center">[George's Blog](https://https://georgech2.github.io/)</p>**
+**<p align="center">无限进步，自由生活~</p>**
 
 ## 联系方式
-- Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
-- Telegram：[@super_leeyom](https://t.me/super_leeyom)
-- Email：[leeyomwang@163.com](mailto:leeyomwang@163.com)
-- Blog：[https://blog.leeyom.top](https://blog.leeyom.top)
+- Twitter：[@georgezh](https://twitter.com/georgezh5)
+- Email：[georgechmr@gmail.com](georgechmr@gmail.com)
+- Blog：[https://https://georgech2.github.io/](https://https://georgech2.github.io/)
 """
 
 BACKUP_DIR = "backup"
